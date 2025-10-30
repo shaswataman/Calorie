1cf59b6a61806dfa1b3aaa1b96518773bbe2b186
Shaswat <callmeshaswat@gmail.com>
2025-10-31 01:35:31 +0530

    decommit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Set correct base for GitHub Pages project site: https://<user>.github.io/Calories/
-  base: '/Calories/',
   plugins: [react()],
   server: {
     port: 3000,
